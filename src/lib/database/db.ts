f77ebe00e0254df91d2e57bff35ffb8750f8fc1b
mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph <mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph>
2022-12-31 21:21:03 +0800

    Debug v5

--- src/lib/database/db.ts
+++ src/lib/database/db.ts
@@ -2,7 +2,7 @@ import mongo from 'mongoose';
 import { MONGO_URL } from "$env/static/private";
 
 export const connect = async() => {
-    await mongo.connect(); 
+    await mongo.connect(MONGO_URL); 
     mongo.Promise = global.Promise;
 }
 export const disconnect = async() => await mongo.disconnect();
