4c9f0bbfd3d6dd2f331f1cfe3770b6d784b27200
mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph <mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph>
2023-03-11 22:13:05 +0800

    Debug v1.2

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -15,14 +15,14 @@ export const actions = {
       const pass = data.get("pass");
 
         const uuID = uuid(); 
-        const user = await users.findOneAndUpdate({
+        /*const user = await users.findOneAndUpdate({
           credentials: {
             email: email,
             password: pass
           }
         }, {
           $set: { authId: uuID }
-        });
+        });*/
 
         cookies.set("session", uuID, {
           path: '/',
@@ -31,7 +31,7 @@ export const actions = {
           maxAge: 60 * 60 * 24 * 30,
         })
 
-        if (user) throw redirect(303, "/");
+        if (true) throw redirect(303, "/");
         else return fail(469, { error: "You gave the wrong credentials." });
 
     }
